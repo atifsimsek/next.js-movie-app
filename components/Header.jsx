9817e5dd6a650a7fe6b257da27ab6c4a75014ef4
Atıf Şimşek <dev.atifsimsek@gmail.com>
2023-02-24 21:40:38 +0300

    bugs fixed

--- components/Header.jsx
+++ components/Header.jsx
@@ -32,9 +32,10 @@ const Header = () => {
     }
   }
 
+
   return (
     <div className='flex items-center gap-5 h-20 p-5 select-none'>
-      <Link href="http://localhost:3000/" className='bg-amber-600 p-3 text-black text-2xl font-bold'>Movie App</Link>
+      <Link href="/" className='bg-amber-600 p-3 text-black text-2xl font-bold'>Movie App</Link>
       <div className='flex flex-1 items-center gap-2 border p-3 rounded-lg'>
         <input value={keyword} onKeyDown={searchHandle} onChange={(e) => { setKeyword(e.target.value) }} placeholder='Search' className=' bg-transparent outline-none flex-1' type="text" />
         <BiSearch className='cursor-pointer' onClick={searchHandle} size={25} />
